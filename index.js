12aa11ca3a6b44287a0976c382f5225304cfe179
ahmed-H20 <ahmedheshamahah8@gmail.com>
2023-09-20 02:02:02 +0300

    Make basic math operators functions
    add() to add 2 (x,y) numbers
    subtract() return sub of numbers
    multiply() function
    and finaly divide()

---
+++ index.js
@@ -0,0 +1,17 @@
+// x => first number
+// y => second number
+function add(x,y) {
+    return x + y;
+}
+
+function subtract(x,y) {
+    return x - y;
+}
+
+function multiply(x,y) {
+    return x * y;
+}
+
+function divide(x,y) {
+    return x / y;
+}
